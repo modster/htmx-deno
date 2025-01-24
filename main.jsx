aea4329a81a797faece8b0c8428f4914debbab51
EM Greeff <emgreeff@datadash.cloud>
2025-01-23 22:01:19 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   main.jsx

--- main.jsx
+++ main.jsx
@@ -3,28 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 let counter = 0;
 
-app.use("/", async (c, next) => {
-  c.setRenderer((content, head) => {
-    return c.html(
-      <html>
-        <head>
-          <title>{head.title ?? ""}</title>
-          <script src="https://unpkg.com/htmx.org@2.0.4"></script>
-        </head>
-        <body>
-          <p>{content}</p>
-        </body>
-      </html>,
-    );
-  });
-  await next();
-});
-
 app.get("/", (c) => {
-  return c.render("Hello!", { title: "Hono SSG Page" });
-});
-
-app.get("/htmx", (c) => {
   return c.html(
     <html>
       <head>
@@ -52,7 +31,7 @@ app.get("/htmx", (c) => {
   );
 });
 
-app.post("/htmx", (c) => {
+app.post("/", (c) => {
   counter++;
   return c.html(
     <div id="parent-div">
