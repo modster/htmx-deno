042338185aa350402b25c89c50f5285a050235ce
EM Greeff <emgreeff@datadash.cloud>
2025-01-23 22:00:02 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   deno.json
    	modified:   main.jsx

--- main.jsx
+++ main.jsx
@@ -1,28 +1,72 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
+let counter = 0;
 
-app.get('/', (c) => c.html('Hello, World!'))
-
-app.use('/about', async (c, next) => {
+app.use("/", async (c, next) => {
   c.setRenderer((content, head) => {
     return c.html(
       <html>
         <head>
-          <title>{head.title ?? ''}</title>
+          <title>{head.title ?? ""}</title>
+          <script src="https://unpkg.com/htmx.org@2.0.4"></script>
         </head>
         <body>
           <p>{content}</p>
         </body>
-      </html>
-    )
-  })
-  await next()
-})
+      </html>,
+    );
+  });
+  await next();
+});
 
-app.get('/about', (c) => {
-  return c.render('Hello!', { title: 'Hono SSG Page' })
-})
+app.get("/", (c) => {
+  return c.render("Hello!", { title: "Hono SSG Page" });
+});
 
-export default app
-Deno.serve({ port: 8000 }, app.fetch) 
+app.get("/htmx", (c) => {
+  return c.html(
+    <html>
+      <head>
+        <title>HTMX Page</title>
+        <script
+          src="https://unpkg.com/htmx.org@2.0.4/dist/htmx.js"
+          integrity="sha384-oeUn82QNXPuVkGCkcrInrS1twIxKhkZiFfr2TdiuObZ3n3yIeMiqcRzkIcguaof1"
+          crossorigin="anonymous"
+        >
+        </script>
+      </head>
+      <body>
+        <div id="parent-div">
+          <button
+            hx-post="/htmx"
+            hx-trigger="click"
+            hx-target="#parent-div"
+            hx-swap="outerHTML"
+          >
+            {counter}
+          </button>
+        </div>
+      </body>
+    </html>,
+  );
+});
+
+app.post("/htmx", (c) => {
+  counter++;
+  return c.html(
+    <div id="parent-div">
+      <button
+        hx-post="/htmx"
+        hx-trigger="click"
+        hx-target="#parent-div"
+        hx-swap="outerHTML"
+      >
+        {counter}
+      </button>
+    </div>,
+  );
+});
+
+export default app;
+Deno.serve({ port: 8000 }, app.fetch);
